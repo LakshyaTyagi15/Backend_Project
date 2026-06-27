20c20cbe7440893d93f9594bfd755da4510310c3
Lakshya Tyagi <lakshyatyagifeb@gmail.com>
2026-06-27 14:52:55 +0530

    Project Started

---
+++ E-commerce/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const ownersRouter = require('./routes/ownerRouter');
+const usersRouter = require('./routes/userRouter');
+const productsRouter = require('./routes/productRouter');
+
+const db = require('./config/mongoose-connection');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('hey');
+});
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+app.listen(3000);
---
+++ E-commerce/models/owners-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const ownerSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    product: {
+        type: Array,
+        default: [],
+    },
+    picture: String,
+    gstin: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
